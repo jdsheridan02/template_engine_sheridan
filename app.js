be1cc23a705028e93fefb1f5066071856e4d1a1a
jdsheridan02 <jdsheridan02@gmail.com>
2020-07-02 13:06:32 -0400

    updated app.js, corrected prompts, and pushed all answers into array to be used by renderHTML

--- app.js
+++ app.js
@@ -9,152 +9,155 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./Library/htmlRenderer");
-
+const employees = [];
 
 // Write code to use inquirer to gather information about the development team members,
+employeeChoice();
 
-function internSurvey (){
-    inquirer.prompt([
-            {
-                type:"input",
-                message: "What is your name?",
-                name: "Name"
-            },
-            {
-                type:"input",
-                message: "What is your ID?",
-                name: "ID"
-            },
-            {
-                type:"input",
-                message: "What is your email?",
-                name: "Email"
-            },
-            {
-                type:"input",
-                message: "What is your role?",
-                name: "Job Title"
-            },
-            {
-                type:"input",
-                message: "What is your role specific property (School, GitHub Link, Office Number)?",
-                name: "Specific Property"
-            },
-])};
+function employeeChoice() {
+    inquirer.prompt([{
+        type: "list",
+        name: "choice",
+        choices: ['Intern', 'Engineer', 'Manager', 'Done'],
+        message: "Please choose one of the following: ",
+    }])
+        .then(function (answers) {
+            if (answers.choice === "Intern") {
+                internSurvey();
+            }
+            if (answers.choice === "Engineer") {
+                engSurvey();
+            }
+            if (answers.choice === "Manager") {
+                manSurvey();
+            }
+            if (answers.choice === "Done") {
+                createHTMLFile();
+            }
+        })
+}
+function internSurvey() {
+    // ask for the name, id, email, and school
+     inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "Name"
+        },
+        {
+            type: "input",
+            message: "What is your ID?",
+            name: "ID"
+        },
+        {
+            type: "input",
+            message: "What is your email?",
+            name: "Email"
+        },
+        {
+            type: "input",
+            message: "What is your role specific property (School, GitHub Link, Office Number)?",
+            name: "Property"
+        },
+    ]).then (function (internAnswers){ 
+        // function is pushing all the answers into an array which will be used by the htmlrender.js file
+        console.log(internAnswers);
 
-function engSurvey (){
-    inquirer.prompt([
-            {
-                type:"input",
-                message: "What is your name?",
-                name: "Name"
-            },
-            {
-                type:"input",
-                message: "What is your ID?",
-                name: "ID"
-            },
-            {
-                type:"input",
-                message: "What is your email?",
-                name: "Email"
-            },
-            {
-                type:"input",
-                message: "What is your role?",
-                name: "Job Title"
-            },
-            {
-                type:"input",
-                message: "What is your role specific property (School, GitHub Link, Office Number)?",
-                name: "Specific Property"
-            },
-])};
+        const intern = new Intern (internAnswers.Name, internAnswers.ID, internAnswers.Email, internAnswers.Property)
+        console.log(intern);
+        employees.push(intern);
+        console.log(employees);
+        
+        // executing initial function for multiple employees, or to finish running the function and build the HTML file
+        employeeChoice();
+    })
+};
 
-function manSurvey (){
+
+
+function engSurvey() {
+    // ask for the name, id, email, and github
+
     inquirer.prompt([
-            {
-                type:"input",
-                message: "What is your name?",
-                name: "Name"
-            },
-            {
-                type:"input",
-                message: "What is your ID?",
-                name: "ID"
-            },
-            {
-                type:"input",
-                message: "What is your email?",
-                name: "Email"
-            },
-            {
-                type:"input",
-                message: "What is your role?",
-                name: "Job Title"
-            },
-            {
-                type:"input",
-                message: "What is your role specific property (School, GitHub Link, Office Number)?",
-                name: "Specific Property"
-            },
-])};
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "Name"
+        },
+        {
+            type: "input",
+            message: "What is your ID?",
+            name: "ID"
+        },
+        {
+            type: "input",
+            message: "What is your email?",
+            name: "Email"
+        },
+        {
+            type: "input",
+            message: "What is your role specific property (School, GitHub Link, Office Number)?",
+            name: "Property"
+        },
+    ]).then (function (engAnswers){ 
+        // function is pushing all the answers into an array which will be used by the htmlrender.js file
+        console.log(engAnswers);
+
+        const engineer = new Engineer (engAnswers.Name, engAnswers.ID, engAnswers.Email, engAnswers.Property)
+        console.log(engineer);
+        employees.push(engineer);
+        console.log(employees);
+
+        // executing initial function for multiple employees, or to finish running the function and build the HTML file
+        employeeChoice();
+    })
+};
+
+
+function manSurvey() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "Name"
+        },
+        {
+            type: "input",
+            message: "What is your ID?",
+            name: "ID"
+        },
+        {
+            type: "input",
+            message: "What is your email?",
+            name: "Email"
+        },
+        {
+            type: "input",
+            message: "What is your role specific property (School, GitHub Link, Office Number)?",
+            name: "Property"
+        },
+    ]).then (function (manAnswers){ 
+        // function is pushing all the answers into an array which will be used by the htmlrender.js file
+        console.log(manAnswers);
+
+        const manager = new Manager (manAnswers.Name, manAnswers.ID, manAnswers.Email, manAnswers.Property)
+        console.log(manager);
+        employees.push(manager);
+        console.log(employees);
+
+        // executing initial function for multiple employees, or to finish running the function and build the HTML file
+        employeeChoice();
+    })
+};
 
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-const intern = new Intern(function(){
-    function getName(){
-        internName = internSurvey[0].input
-    };
-    function getId(){
-        internId = internSurvey[1].input
-    };
-    function getEmail(){
-        internEmail = internSurvey[2].input
-    };
-    function getRole(){
-        internRole = internSurvey[3].input
-    };
-    function getSchool(){
-        internSchool = internSurvey[4].input
-    }
-});
+function createHTMLFile(){
+    const html = render(employees);
+    console.log(html);
 
-const engineer = new Engineer(function(){
-    function getName(){
-        engName = engSurvey[0].input
-    };
-    function getId(){
-        engId = engSurvey[1].input
-    };
-    function getEmail(){
-        engEmail = engSurvey[2].input
-    };
-    function getRole(){
-        engRole = engSurvey[3].input
-    };
-    function getGitHub(){
-        engGit = engSurvey[4].input
-    }
-});
-
-const manager = new Manager(function(){
-    function getName(){
-        manName = manSurvey[0].input
-    };
-    function getId(){
-        manId = manSurvey[1].input
-    };
-    function getEmail(){
-        manEmail = manSurvey[2].input
-    };
-    function getRole(){
-        manRole = manSurvey[3].input
-    };
-    function getOfficeNum(){
-        manOffice = manSurvey[4].input
-    }
-});
+    fs.writefile
+};
 
 // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
 
@@ -167,35 +170,6 @@ const manager = new Manager(function(){
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different information; write your code to ask different questions via inquirer depending on employee type.
 
-// function employeeChoice() {
-//     inquirer.prompt([{
-//       type: ‘list’,
-//       name: ‘choice’,
-//       choices: [‘Intern’, ‘Engineer’, ‘Done’],
-//       message: ‘Please choose one of the following: ’,
-//     }])
-//       .then(function(answers) {
-//         if (answers.choice === ‘Intern’) {
-//           createIntern();
-//         }
-//         if (answers.choice === ‘Engineer’) {
-//           createEngineer()
-//         }
-//         if (answers.choice === ‘Done’) {
-//           createHTMLFile();
-//         }
-//       })
-//   }
-//   function createIntern() {
-//     // ask for the name, id, email, and school
-//     // push it to teamMembers
-//     // call employeeChoice
-//   }
-//   function createEngineer() {
-//     // ask for the name, id, email, and github
-//     // push it to teamMembers
-//     // call employeeChoice
-//   }
 
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
