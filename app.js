e38a2c39e44bb977208a7ee95224daec7c31394d
jdsheridan02 <jdsheridan02@gmail.com>
2020-07-03 08:52:07 -0400

    added screen recording demo of app

--- app.js
+++ app.js
@@ -165,18 +165,3 @@ function createHTMLFile(){
     fs.writeFileSync(outputPath, render(employees), "utf-8")
 
 };
-
-
-
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
-
-// Hint: you may need to check if the `output` folder exists and create it if it does not.
-
-
-
-
-
-
-
